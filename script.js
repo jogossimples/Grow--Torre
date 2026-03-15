674c55cea181941282202110e09fc424c53c7049
jogossimples <githubjogos@gmail.com>
2026-03-15 13:11:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Lógica do jogo
+let jogo = new Phaser.Game({
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+});
+
+function preload() {
+  // Carregar recursos
+}
+
+function create() {
+  // Criar a torre e os inimigos
+}
+
+function update() {
+  // Atualizar a lógica do jogo
+}
